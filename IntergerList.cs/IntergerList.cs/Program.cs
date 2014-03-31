56b8b798247b04f6cda95a495f926720804566ce
william raisner <raisnerw@gmail.com>
2014-03-31 10:06:10 -0400

    hhh

---
+++ IntergerList.cs/IntergerList.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 1, 5, 3, 2, 4 };
+
+            Array.Sort(num);
+            for (int i = 0; i < 5; i++)
+            {
+
+                // 
+                Console.WriteLine(num[i]);
+            }
+
+            Array.Reverse(num);
+
+            for (int i = 0; i < 5; i++)
+            {
+                //
+                Console.WriteLine(num[i]);
+            }
+
+
+        }
+    }
+}
